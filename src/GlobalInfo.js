af14899bdf2f21eb1e924dadbe78807eddc602be
huanngzh <huanngzh@gmail.com>
2021-08-19 19:52:23 +0800

    修改后端地址

--- src/GlobalInfo.js
+++ src/GlobalInfo.js
@@ -3,5 +3,5 @@ export default class GlobalInfo {
 
     static baseUrl = 'http://localhost:8080';    //前端网址路径，若部署服务器，需做相应修改
     static email = 'zewancc@126.com';          //编辑或管理员邮箱地址
-    static backUrl = 'http://localhost:8000/';  //填写您部署的后端网址
+    static backUrl = 'https://zewan.cc/api/pt/';  //填写您部署的后端网址
 }
