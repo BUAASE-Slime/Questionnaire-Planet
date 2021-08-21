39ed6f0bba1995fd2209c137cc5ac8dd1794d3c7
Zehuan Huang <zewanhuang@126.com>
2021-08-21 14:31:03 +0800

    qn stat

--- src/views/qn-stat/QnStat.vue
+++ src/views/qn-stat/QnStat.vue
@@ -1,33 +1,38 @@
 <template>
-  <div>
-    <el-col :span="6" class="left-nav">
-      <el-menu
-          default-active="1"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose">
-        <el-menu-item index="1">
-          <i class="el-icon-time nav-icon"></i>
-          <br>
-          <span slot="title">回收概况</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <i class="el-icon-s-data nav-icon"></i>
-          <br>
-          <span slot="title">图表报告</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-data-analysis nav-icon"></i>
-          <br>
-          <span slot="title">交叉分析</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-download nav-icon"></i>
-          <br>
-          <span slot="title">下载答卷</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
+  <div class="all-body">
+    <el-container>
+      <el-aside class="left-nav">
+        <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose">
+          <el-menu-item index="1">
+            <i class="el-icon-time nav-icon"></i>
+            <br>
+            <span slot="title">回收概况</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-s-data nav-icon"></i>
+            <br>
+            <span slot="title">图表报告</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-data-analysis nav-icon"></i>
+            <br>
+            <span slot="title">交叉分析</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-download nav-icon"></i>
+            <br>
+            <span slot="title">下载答卷</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <h1>sadas</h1>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -37,6 +42,7 @@ export default {
   data() {
     return {
       qnId: '',
+      qnTitle: '问卷标题',
     };
   },
   methods: {
@@ -50,6 +56,10 @@ export default {
 </script>
 
 <style scoped>
+.left-nav {
+  width: 240px;
+  /*margin-left: 50px;*/
+}
 .left-nav .el-menu-item {
   font-size: 16px;
   padding: 20px;
@@ -62,5 +72,9 @@ export default {
 
 .left-nav .nav-icon {
   font-size: 50px;
+}
+
+.all-body {
+  height: 100%;
 }
 </style>
