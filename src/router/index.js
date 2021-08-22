58b2c1444bca66ee465a21c45394569591102f81
MissTini <tongyuning2019@126.com>
2021-08-22 20:35:50 +0800

    回收数据与下载页面

--- src/router/index.js
+++ src/router/index.js
@@ -91,12 +91,12 @@ const routes = [
                 
                 component: () => import('../views/qn-stat/sub-stat/crossover')              
             },
-            {
-                path: '/download',
-                name: 'download',
+            // {
+            //     path: '/download',
+            //     name: 'download',
                 
-                component: () => import('../views/qn-stat/sub-stat/download')              
-            },
+            //     component: () => import('../views/qn-stat/sub-stat/download')              
+            // },
         ]
     },
     {
--- src/views/qn-stat/sub-stat/crossover.vue
+++ src/views/qn-stat/sub-stat/crossover.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>交叉分析</h1>
+    <div id='sum'>
+        <div><h1>交叉分析</h1></div>
+    </div>
 </template>
+
+<style scoped>
+    #sum{
+        width: 90%;
+        /* background-color: cyan; */
+    }
+</style>
