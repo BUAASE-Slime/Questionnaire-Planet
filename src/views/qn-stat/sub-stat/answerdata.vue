ccec958353e96cdcb3941f8d4558962864175f88
MissTini <tongyuning2019@126.com>
2021-08-26 17:54:59 +0800

    答卷结果预览

--- src/views/qn-stat/sub-stat/answerdata.vue
+++ src/views/qn-stat/sub-stat/answerdata.vue
@@ -4,7 +4,7 @@
         <el-card class="box-card">
         <div>
               <div>
-              <el-button type="primary" @click="downloadExcel" style="float: right;margin: 30px;">下载</el-button>
+              <el-button id="down" type="primary" @click="downloadExcel" style="float: right;margin: 30px;">下载</el-button>
               </div>
               <div>
               <el-table
@@ -39,10 +39,10 @@
                     <el-table-column label="操作" width="250" align="center">
                         <template slot-scope="scope">
                           <div style="display:inline">
-                            <el-button type="text" class="el-icon-delete" style="color: red" @click="delSubmit(scope.row.submit_id, scope.row.num)">删除</el-button>
+                            <el-button type="text" class="el-icon-delete op" style="color: red" @click="delSubmit(scope.row.submit_id, scope.row.num)">删除</el-button>
                           </div>
                           <div style="display:inline">
-                            <el-button type="text" class="el-icon-document" style="color: black">预览</el-button>
+                            <el-button type="text" class="el-icon-document op" style="color: black " @click="openBox(scope.row.num)">预览</el-button>
                           </div>
                         </template>
                     </el-table-column>
@@ -60,6 +60,75 @@
                 </el-pagination>
                 </div>
         </div>
+        <div>
+        <el-dialog
+          :title='"答卷序号："+indexnum'
+          :visible.sync="dialogVisible"
+          width="50%"
+          >
+          <div id="dia">
+          <div class="main">
+          <div class="title" style="clear:both">
+            {{ title }}
+          </div>
+
+          <div class="description" v-if="this.description!==''">
+            &emsp;&emsp;{{ description }}
+          </div>
+
+          <el-divider></el-divider>
+          <div class="ques-block" v-for="item in questions" :key="item.id">
+
+            <div class="q-title">
+              {{ item.id }}. {{ item.title }} <span class="must" v-if="item.must">(必填)</span>
+            </div>
+
+            <div
+                class="q-description"
+                v-if="item.description!=='' && item.description!==null && item.description!==undefined"
+            >
+              {{ item.description }}
+            </div>
+
+            <div v-if="item.type==='radio'">
+              <div class="q-opt" v-for="opt in item.options" :key="opt.id">
+                <el-radio v-if="item.type==='radio'" v-model="answers[item.id-1].answer" :label="opt.title">
+                  {{ opt.title }}
+                </el-radio>
+              </div>
+            </div>
+
+            <el-checkbox-group class="q-opt" v-if="item.type==='checkbox'" v-model="answers[item.id-1].ansList">
+              <el-checkbox v-for="opt in item.options" :key="opt.id" :label="opt.title">
+                {{ opt.title }}
+              </el-checkbox>
+            </el-checkbox-group>
+
+            <div class="q-opt" v-if="item.type==='text'">
+              <el-input
+                  type="textarea"
+                  :autosize="{ minRows: 2, maxRows: item.row}"
+                  placeholder="请输入内容"
+                  v-model="answers[item.id-1].ans"
+                  readonly="true">
+              </el-input>
+            </div>
+
+            <div class="q-opt" v-if="item.type==='mark'">
+              <el-rate disabled show-score text-color="#ff9900" v-model="answers[item.id-1].ans" :max="item.score"></el-rate>
+            </div>
+
+          </div>
+
+          </div>
+          <span slot="footer" class="dialog-footer">
+            <el-button v-if="indexnum>1"  @click="frontpage">上一页</el-button>
+            <el-button v-if="indexnum<tableData.length"  @click="nextpage">下一页</el-button>
+            <el-button type="primary" @click="dialogVisible = false">返 回</el-button>
+          </span>
+          </div>
+        </el-dialog>
+        </div>
         </el-card>
     </div>
 </template>
@@ -68,109 +137,321 @@
   export default {
     data() {
       return {
-        
+        title:"小学期问卷",
+        description:"愉快的小学期",
+        answers:[
+            {
+                question_id: 66,
+                type: "radio",
+                ans: "问卷星球",
+                ansList: [],
+                answer: "问卷星球"
+            },
+            {
+                question_id: 67,
+                type: "checkbox",
+                ans: null,
+                ansList: [
+                    "ZXH",
+                    "ZYH",
+                    "HZY"
+                ],
+                answer: "ZXH-<^-^>-ZYH-<^-^>-HZY"
+            },
+            {
+                question_id: 68,
+                type: "radio",
+                ans: "感受不到",
+                ansList: [],
+                answer: "感受不到"
+            },
+            {
+                question_id: 69,
+                type: "radio",
+                ans: "1-3小时",
+                ansList: [],
+                answer: "1-3小时"
+            },
+            {
+                question_id: 70,
+                type: "mark",
+                ans: 9,
+                ansList: [],
+                answer: "9"
+            },
+            {
+                question_id: 230,
+                type: "text",
+                ans: "填空题回答",
+                ansList: [],
+                answer: "填空题回答"
+            }
+        ],
+        questions: [
+            {
+                question_id: 66,
+                row: 1,
+                score: 10,
+                title: "小学期开发的内容是？",
+                description: "看看你们是不是还没看需求",
+                must: true,
+                type: "radio",
+                qn_id: 18,
+                sequence: 1,
+                option_num: 14,
+                refer: "",
+                point: 0,
+                id: 1,
+                options: [
+                    {
+                        id: 762,
+                        title: "问卷星球"
+                    },
+                    {
+                        id: 763,
+                        title: "出版系统"
+                    }
+                ],
+                answer: ""
+            },
+            {
+                question_id: 67,
+                row: 1,
+                score: 10,
+                title: "本次小学期的助教有？",
+                description: "不会吧不会吧，不会有人真以为助教只是助教吧？",
+                must: true,
+                type: "checkbox",
+                qn_id: 18,
+                sequence: 2,
+                option_num: 35,
+                refer: "",
+                point: 0,
+                id: 2,
+                options: [
+                    {
+                        id: 764,
+                        title: "ZXH"
+                    },
+                    {
+                        id: 765,
+                        title: "ZYH"
+                    },
+                    {
+                        id: 766,
+                        title: "HZY"
+                    },
+                    {
+                        id: 767,
+                        title: "ZHT"
+                    },
+                    {
+                        id: 768,
+                        title: "LKW"
+                    }
+                ],
+                answer: ""
+            },
+            {
+                question_id: 68,
+                row: 1,
+                score: 10,
+                title: "敏捷开发中你感受得到一丝丝快乐吗？",
+                description: "",
+                must: false,
+                type: "radio",
+                qn_id: 18,
+                sequence: 3,
+                option_num: 14,
+                refer: "",
+                point: 0,
+                id: 3,
+                options: [
+                    {
+                        id: 769,
+                        title: "感受不到"
+                    },
+                    {
+                        id: 770,
+                        title: "一丝丝都感受不到"
+                    }
+                ],
+                answer: ""
+            },
+            {
+                question_id: 69,
+                row: 1,
+                score: 10,
+                title: "第一次迭代验收前每天平均睡眠时间",
+                description: "有人在第一次验收前夜通宵了吗，我看到 JBW 鏖战了20多小时",
+                must: true,
+                type: "radio",
+                qn_id: 18,
+                sequence: 4,
+                option_num: 28,
+                refer: "",
+                point: 0,
+                id: 4,
+                options: [
+                    {
+                        id: 771,
+                        title: "1-3小时"
+                    },
+                    {
+                        id: 772,
+                        title: "3-5小时"
+                    },
+                    {
+                        id: 773,
+                        title: "5-7小时"
+                    },
+                    {
+                        id: 774,
+                        title: "睡啥觉，起来敲代码"
+                    }
+                ],
+                answer: ""
+            },
+            {
+                question_id: 70,
+                row: 1,
+                score: 10,
+                title: "小学期作为乙方的感受",
+                description: "请谨慎回答",
+                must: true,
+                type: "mark",
+                qn_id: 18,
+                sequence: 5,
+                option_num: 1,
+                refer: "",
+                point: 0,
+                id: 5,
+                options: [],
+                answer: ""
+            },
+            {
+                question_id: 230,
+                row: 2,
+                score: 5,
+                title: "这是一道填空题",
+                description: "这是描述",
+                must: false,
+                type: "text",
+                qn_id: 18,
+                sequence: 6,
+                option_num: 1,
+                refer: "",
+                point: 0,
+                id: 6,
+                options: [],
+                answer: ""
+            }
+        ],
+        indexnum: 0,
+        dialogVisible: false,
         currentPage: 1,
         pageSize: 5,
         pageSizes:[3,5,7],
-        loading: true,
+        loading: false,
         tableData: [
-        // {
-        // num: 1,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 2,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 3,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 4,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },       
-        // {
-        // num: 5,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 6,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 7,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 8,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },   
-        // {
-        // num: 9,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 10,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // }, 
-        // {
-        // num: 11,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 12,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 13,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 14,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },       
-        // {
-        // num: 15,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },
-        // {
-        // num: 16,
-        // submit_time:'2021/4/7 19:34:42',
-        // answer_num:10,
-        // answer_percent:'70%',
-        // },     
-        ]
+        {
+        num: 1,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 2,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 3,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 4,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },       
+        {
+        num: 5,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 6,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 7,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 8,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },   
+        {
+        num: 9,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 10,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        }, 
+        {
+        num: 11,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 12,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 13,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 14,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },       
+        {
+        num: 15,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },
+        {
+        num: 16,
+        submit_time:'2021/4/7 19:34:42',
+        answer_num:10,
+        answer_percent:'70%',
+        },     
+        ],
+
       }
     },
     created() {
@@ -196,7 +477,23 @@
         console.log(err);
       })
     },
+
     methods: {
+      nextpage(){
+        let ele=document.getElementById('dia');
+        ele.scrollTop = 0;
+        this.indexnum=this.indexnum+1;
+      },
+      frontpage(){
+        let ele=document.getElementById('dia');
+        ele.scrollTop = 0;
+        this.indexnum=this.indexnum-1;
+      },
+      openBox(index) {
+      this.indexnum=index;
+      this.dialogVisible = true;
+      console.log(this.indexnum);
+      },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
         this.currentPage = 1;
@@ -277,11 +574,66 @@
          width: 90%;
         /* background-color: cyan; */
  }
- .el-button{
+ .op{
     float: right;
     margin: 5px 30px;
  }
  .box-card{
      margin: 20px;
  }
+.must {
+  font-weight: normal;
+  color: crimson;
+}
+ .main .el-radio{
+   pointer-events: none;
+}
+.main .el-checkbox{
+  pointer-events: none;
+}
+#dia{
+  height: 500px;
+  overflow: auto;
+}
+
+.q-title {
+  text-align: left;
+  /*border: solid 1px black;*/
+  font-size: 16px;
+  padding: 40px 10px 10px;
+  font-weight: bold;
+}
+.q-description {
+  text-align: left;
+  font-size: 14px;
+  padding-left: 10px;
+  padding-top: 5px;
+  padding-bottom: 10px;
+  color: #969696;
+}
+.q-opt {
+  text-align: left;
+  /*border: solid 1px black;*/
+  font-size: 16px;
+  padding: 10px 10px 10px;
+}
+.el-checkbox {
+  padding: 10px 0;
+  display: block;
+}
+.title {
+  font-size: 28px;
+  font-weight: bold;
+  padding-bottom: 20px;
+  padding-top: 25px;
+}
+
+.description {
+  text-align: left;
+  font-size: 16px;
+  color: #969696;
+  line-height: 30px;
+  padding-bottom: 10px;
+}
+
 </style>
