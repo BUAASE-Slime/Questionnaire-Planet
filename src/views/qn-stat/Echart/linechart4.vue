2d6497c9ba7f6701feedc33e1af3621c3cb84b9a
MissTini <tongyuning2019@126.com>
2021-08-28 09:43:55 +0800

    交叉分析表格（数据未连接）

---
+++ src/views/qn-stat/Echart/linechart4.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="page_view">
+    <div id="myChart4" class="chart-view"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'echart',
+  props: {
+    chart4_name:null,
+    chart4_data:null,
+  },
+  data () {
+    return {
+      legend_data:['a','b','c','d'],
+      x_data:['d','b','c'],
+      series_data:[
+            {
+                name: 'a',
+                type: 'line',
+                data: [0,33.33,20]
+            },
+            {
+                name: 'b',
+                type: 'line',
+                data: [66.66,0,0]
+            },
+            {
+                name: 'c',
+                type: 'line',
+                data: [33.33,66.66,20]
+            },
+            {
+                name: 'd',
+                type: 'line',
+                data: [0,0,60]
+            },
+        ]
+    }
+  },
+  mounted(){
+    //页面加载完成后,才执行
+    setTimeout(() => {
+      this.showChart4();
+    }, 500);
+  },
+  methods: {
+ 
+    showChart4()
+    {
+      var myChart4= this.$echarts.init(document.getElementById('myChart4'));
+      let option4 = {
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: this.legend_data
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: this.x_data,
+                },
+                yAxis: {
+                    type: 'value',
+                    axisLabel: {
+                        formatter: '{value} %'
+                    }
+                },
+                series: this.series_data
+        };
+         myChart4.setOption(option4)
+    }
+  }
+}
+</script>
+
+<style>
+
+.page_view
+{
+  padding:20px 3%;
+  text-align: center;
+}
+
+.title{
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.chart-view{
+  margin: 20px auto;
+  width: 400px;
+  height: 400px;
+}
+
+</style>
+
