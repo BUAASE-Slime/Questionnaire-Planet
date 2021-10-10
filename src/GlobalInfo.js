a13aa094f6a7df2e58e55f01b7c3e66edf0d0266
huanngzh <huanngzh@gmail.com>
2021-10-11 00:29:56 +0800

    update globalinfo for deploy

--- src/GlobalInfo.js
+++ src/GlobalInfo.js
@@ -1,8 +1,8 @@
 
 export default class GlobalInfo {
     //---------------------------------域名服务器配置--------------------------------//
-    static baseUrl = 'http://localhost:8080';               //前端网址路径，若部署服务器，需做相应修改
-    static backUrl = 'http://localhost:8080/api/api/qs/';   //后端网址路径，若部署服务器，需做相应修改
+    static baseUrl = 'https://qn.zewan.cc';                 //前端网址路径，若部署服务器，需做相应修改
+    static backUrl = 'https://qn.zewan.cc/api/qs/';         //后端网址路径，若部署服务器，需做相应修改
     static email = 'zewancc@126.com';                       //编辑或管理员邮箱地址
     static domain = 'qn.zewan.cc';                          //底边栏显示的域名
     static publicBackUrl = 'https://qn.zewan.cc/api/qs/';   //公开后端网址路径，主要用于获取用户IP地址
